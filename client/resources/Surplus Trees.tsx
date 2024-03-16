d9a3ad71fd589c9cb130ab4f9f9e207262bcea76
Ragnapop <jinhunl@gmail.com>
2024-03-16 21:13:59 +0800

    Update Tilemaps

---
+++ client/resources/Surplus Trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Surplus Trees" tilewidth="16" tileheight="16" tilecount="2450" columns="50">
+ <image source="../../../../../Downloads/Surplus Trees.png" width="800" height="785"/>
+</tileset>
