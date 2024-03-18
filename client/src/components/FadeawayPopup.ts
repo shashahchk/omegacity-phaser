41da43217b85c619417e13d9c6526b03fb78cbec
Kin Tat <110956850+KinTatHo@users.noreply.github.com>
2024-03-18 17:26:12 +0800

    Guided caption now interactive

--- client/src/components/FadeawayPopup.ts
+++ client/src/components/FadeawayPopup.ts
@@ -7,46 +7,53 @@ export class FadeawayPopup {
   private characterSprite: Phaser.GameObjects.Sprite | null = null;
   private textBubble: Phaser.GameObjects.Sprite | null = null;
   private background: Phaser.GameObjects.Sprite | null = null;
+  private currentIndex: number = 0; // Add a currentIndex to keep track of the current caption
+  private texts: string[] = []; // Store texts array
+  private x: number = 0;
+  private y: number = 0;
+  private characterImage: string = "";
+  private textImage: string = "";
+  private backgroundImage: string = "";
 
   constructor(scene: Scene) {
     this.scene = scene;
+    this.x = this.scene.cameras.main.centerX;
+    this.y = this.scene.cameras.main.centerY;
   }
 
-  private async create(
-    x: number,
-    y: number,
-    text: string,
-    characterImage: string,
-    textImage: string,
-    backgroundImage: string
-  ): Promise<void> {
+  preload() {
+    this.scene.load.image("narrator", "ui/cuter-narrator.png");
+    this.characterImage = "narrator";
+    this.scene.load.image("textBubble", "ui/pixel-speech.png");
+    this.textImage = "textBubble";
+    this.scene.load.image("background", "ui/start-background.png");
+    this.backgroundImage = "background";
+  }
+
+  private async create(): Promise<void> {
     return new Promise((resolve) => {
       this.destroy();
-      this.background = this.scene.add
-        .sprite(
-          0,
-          0,
-          backgroundImage
-        );
-      this.background.setOrigin(0, 0); // Set the origin to the top left corner
+      this.background = this.scene.add.sprite(0, 0, this.backgroundImage);
+      this.background.setOrigin(0, 0).setScale(0.8).setAlpha(0.25);
 
-      this.characterSprite = this.scene.add.sprite(x, y - 20, characterImage);
+      this.characterSprite = this.scene.add.sprite(this.x, this.y - 20, this.characterImage);
 
       this.textBubble = this.scene.add.sprite(
-        x + this.characterSprite.width,
-        y - 200,
-        textImage
+        this.x + this.characterSprite.width,
+        this.y - 200,
+        this.textImage
       );
 
-      this.textBubble.setScale(1.2).setDepth(1).setOrigin(0, 0);
+      this.textBubble.setScale(1.2).setDepth(1).setOrigin(0, 0).setInteractive();
 
       this.characterSprite.setScale(1.5).setDepth(0).setOrigin(0, 0);
       this.caption = this.scene.add.text(
-        x + this.characterSprite.width * 1.25,
-        y - 50,
-        text,
+        this.x + this.characterSprite.width * 1.25,
+        this.y - 50,
+        this.texts[this.currentIndex],
         {
-          font: "20px Press Start 2P",
+          fontFamily: '" Press Start 2P", cursive',
+          font: "20px",
           color: "black",
           align: "center",
           backgroundColor: "white",
@@ -54,24 +61,32 @@ export class FadeawayPopup {
         }
       );
 
-      this.caption.setDepth(1);
+      this.caption.setDepth(1).setInteractive();
 
       this.fadeAway = this.scene.tweens.add({
         targets: this.caption,
         alpha: { from: 1, to: 0 },
         ease: "Linear",
-        duration: 3000,
+        duration: 5000,
         repeat: 0,
         yoyo: false,
         onComplete: () => {
-          this.destroy();
-          resolve();
+          this.nextCaption();
         },
       });
 
       // Start the tween
       this.fadeAway.play();
     });
+  }
+
+  async nextCaption(): Promise<void> {
+    this.currentIndex++;
+    if (this.currentIndex < this.texts.length) {
+      await this.create();
+    } else {
+      this.destroy();
+    }
   }
 
   async createGuide(
@@ -82,9 +97,9 @@ export class FadeawayPopup {
     textImage: string,
     backgroundImage: string
   ): Promise<void> {
-    for (let text of texts) {
-      await this.create(x, y, text, characterImage, textImage, backgroundImage);
-    }
+    this.texts = texts;
+    this.currentIndex = 0;
+    await this.create();
   }
 
   destroy() {
